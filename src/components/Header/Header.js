c8b46d70b2827084745c543ee283e0f0774d54bc
Durdona Ibragimova <durdona.dev@gmail.com>
2023-11-19 23:59:54 -0500

    Create Header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,39 @@
+import logoWhite from "../../assets/logo-white.svg";
+import cart from "../../assets/cart.svg";
+import "./Header.css";
+
+export const Header = () => {
+    <header className="header">
+        <nav className="navigation">
+            <img src={logoWhite} alt="DeliziOso" className="logo-white" />
+            <ul className="navigation__links">
+                <li>
+                    <a href="" id="navigation__link">
+                        Home
+                    </a>
+                </li>
+                <li>
+                    <a href="">Menu</a>
+                </li>
+                <li>
+                    <a href="">About Us</a>
+                </li>
+                <li>
+                    <a href="">Reservation</a>
+                </li>
+                <li>
+                    <a href="">Contact Us</a>
+                </li>
+                <li>
+                    <a href="./branding.html">Branding</a>
+                </li>
+            </ul>
+            <div className="navigation__actions">
+                <figure>
+                    <img src={cart} alt="Shopping Cart" />
+                </figure>
+                <button className="btn btn-sm btn-green">Sign In</button>
+            </div>
+        </nav>
+    </header>;
+};
