c3185d27dd9a6ecc52702c63861ae7f1b5430926
Durdona Ibragimova <durdona.dev@gmail.com>
2023-11-21 23:05:19 -0500

    Create Welcome Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import { Header } from "./components/Header/Header.js";
 import { Hero } from "./components/Hero/Hero.js";
+import { Welcome } from "./components/Welcome/Welcome.js";
 
 function App() {
     return (
         <>
             <Header />
             <Hero />
+            <Welcome />
         </>
     );
 }
