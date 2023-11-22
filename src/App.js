404f92a295c6c7a6361cb05b32c174351cccfd1d
Durdona Ibragimova <durdona.dev@gmail.com>
2023-11-22 07:16:13 -0500

    Create Chefs Component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Hero } from "./components/Hero/Hero.js";
 import { Welcome } from "./components/Welcome/Welcome.js";
 import { Menu } from "./components/Menu/Menu.js";
 import { Reservation } from "./components/Reservation/Reservation.js";
+import { Chefs } from "./components/Chefs/Chefs.js";
 
 function App() {
     return (
@@ -13,6 +14,7 @@ function App() {
             <Welcome />
             <Menu />
             <Reservation />
+            <Chefs />
         </>
     );
 }
---
+++ src/components/Chefs/Chefs.js
@@ -0,0 +1,41 @@
+import React from "react";
+import "./Chefs.css";
+import ferry from "../../assets/ferry.png";
+import iswan from "../../assets/iswan.png";
+import betran from "../../assets/betran.png";
+
+class Chefs extends React.Component {
+    render() {
+        return (
+            <section id="chefs-section">
+                <div className="chefs-section__container container">
+                    <h2>
+                        Our Greatest <span>Chefs</span>
+                    </h2>
+                    <div className="chefs-section__chefs">
+                        <div className="chefs-section__chef">
+                            <img src={betran} alt="Betran Chef" />
+                            <h4>Betrann Komar</h4>
+                            <p>Head Chef</p>
+                        </div>
+
+                        <div className="chefs-section__chef">
+                            <img src={ferry} alt="Ferry Chef" />
+                            <h4>Ferry Sauwi</h4>
+                            <p>Chef</p>
+                        </div>
+
+                        <div className="chefs-section__chef">
+                            <img src={iswan} alt="Iswan Chef" />
+                            <h4>Iswan Dracho</h4>
+                            <p>Chef</p>
+                        </div>
+                    </div>
+                    <button className="btn btn-lg btn-orange">More</button>
+                </div>
+            </section>
+        );
+    }
+}
+
+export { Chefs };
